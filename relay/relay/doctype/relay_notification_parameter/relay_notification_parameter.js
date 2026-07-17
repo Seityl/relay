b79d39eb7ddd3da79fa852b23423baebfa70ad04
Jeriel Francis <jeriel@seityl.com>
2026-07-17 18:56:25 +0000

    chore: add Frappe-standard scaffolding (JS form scripts, tests, public, fixtures, www)

---
+++ relay/relay/doctype/relay_notification_parameter/relay_notification_parameter.js
@@ -0,0 +1,7 @@
+// Copyright (c) 2026, Jeriel Francis (trading as Seityl) and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on("Relay Notification Parameter", {
+	refresh(frm) {
+	}
+});
---
+++ relay/relay/doctype/relay_webhook_log/relay_webhook_log.js
@@ -0,0 +1,7 @@
+// Copyright (c) 2026, Jeriel Francis (trading as Seityl) and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on("Relay Webhook Log", {
+	refresh(frm) {
+	}
+});
